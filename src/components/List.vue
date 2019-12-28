ab2d75b0ab2ca2e2760b3422bb60e59eef0aa49b
Eros Campos <eroscamposg@gmail.com>
2019-12-27 21:51:08 -0500

    First push

---
+++ src/components/List.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <ul class="list-group">
+      <li
+        v-for="(fruit, index) in arrangedFruits"
+        :key="fruit.id"
+        @click="add(index)"
+        class="list-group-item d-flex justify-content-between align-items-center"
+      >
+        {{ index }} - {{ fruit.name }}
+        <span class="badge badge-primary badge-pill">
+          {{ fruit.quantity }}
+        </span>
+      </li>
+    </ul>
+    <button
+      type="button"
+      class="btn btn-danger btn-block mt-4"
+      @click="resetValues"
+    >
+      Reset values
+    </button>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from "vuex";
+
+export default {
+  name: "List",
+  computed: {
+    ...mapState(["fruits"]),
+    arrangedFruits(state) {
+      return state.fruits.sort((a, b) => b.quantity - a.quantity);
+    }
+  },
+  methods: {
+    ...mapMutations(["add", "resetValues"])
+  }
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,26 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    fruits: [
+      { name: "Manzana", quantity: 0 },
+      { name: "Pera", quantity: 0 },
+      { name: "Naranja", quantity: 0 }
+    ]
   },
   mutations: {
+    add(state, index) {
+      state.fruits[index].quantity++;
+    },
+    resetValues(state) {
+      state.fruits.forEach(fruit => {
+        fruit.quantity = 0;
+      });
+    }
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {}
+});
